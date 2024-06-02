9a3f1a921cb29127a6dae3b3fb3328d8181ec99a
dolydev <dalila.drouiche88@gmail.com>
2024-06-02 03:15:01 +0200

    correction jenkinsfile

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -21,3 +21,13 @@ source /etc/environment
 # Vérifier l'installation
 java --version
 echo $JAVA_HOME
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+#recuperer le password pour l'interface graphic
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
