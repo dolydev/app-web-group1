e6afedf087ccad16ffddb040e808c917d128402d
dolydev <115890261+dolydev@users.noreply.github.com>
2024-06-03 22:12:22 +0200

    Update stagiaires.sql

--- mysql/stagiaires.sql
+++ mysql/stagiaires.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS stagiaires (
     prenom VARCHAR(255) NOT NULL,
     formation VARCHAR(255) NOT NULL
 );
+INSERT INTO stagiaires (nom, prenom, email, date_naissance) VALUES
+('Doe', 'John', 'john.doe@example.com', '1990-01-01'),
+('Smith', 'Jane', 'jane.smith@example.com', '1992-02-02');
