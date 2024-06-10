455efa5f15ad95f412a5ebcc725fc2f12d799ec2
dolydev <dalila.drouiche88gmail.com>
2024-06-10 22:24:14 +0200

    service swarm

--- serviceswarm.sh
+++ serviceswarm.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-# Variables
-IMAGE_NAME="dalila854/image-file-rouge"# Nom de l'image Docker
-IMAGE_TAG="latest" # Tag de l'image Docker
-SERVICE_NAME="serviceswarm" # Nom du service Docker
-PORT_MAPPING="8088:80" # Mappage de port pour le service Docker
-WORKER_NODES=("192.168.176.129") # Liste des nœuds de travail
+echo "Nom : $0"
 
-# Télécharger l'image sur chaque nœud
 echo "Téléchargement de l'image sur le nœud principal..."
-docker pull $IMAGE_NAME:$IMAGE_TAGo
+docker pull dalila854/image-file-rouge:latest || { echo "Échec du téléchargement de l'image"; exit 1; }
 
-for NODE in "${WORKER_NODES[@]}"; do
-    echo "Téléchargement de l'image sur $NODE..."
-    ssh vagrant@$NODE "docker pull $IMAGE_NAME:$IMAGE_TAG"
-done
+echo "Téléchargement de l'image sur 192.168.176.129..."
+ssh -o StrictHostKeyChecking=no user@192.168.176.129 "docker pull dalila854/image-file-rouge:latest" || { echo "Échec du téléchargement de l'image sur 192.168.176.129"; exit 1; }
 
-# Vérifier si le service existe déjà
-SERVICE_EXISTS=$(docker service ls --filter name=$SERVICE_NAME -q)
-
-if [ ! -z "$SERVICE_EXISTS" ]; then
-    echo "Le service $SERVICE_NAME existe déjà. Suppression en cours..."
-    docker service rm $SERVICE_NAME
-    # Attendre que le service soit complètement supprimé
-    sleep 10
-fi
-
-# Créer le service
 echo "Création du service Docker..."
-docker service create --name $SERVICE_NAME -p $PORT_MAPPING --replicas 1 $IMAGE_NAME:$IMAGE_TAG
+docker service create --name site-greta --replicas 1 --publish 80:80 dalila854/image-file-rouge:latest || { echo "Échec de la création du service"; exit 1; }
 
-# Vérifier si l'image est présente sur tous les nœuds
 echo "Vérification de l'image sur tous les nœuds..."
-echo "Nœud principal :"
-docker images | grep $IMAGE_NAME
+docker service ps site-greta || { echo "Échec de la vérification du service"; exit 1; }
